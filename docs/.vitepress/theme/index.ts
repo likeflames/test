b4278ecb0ebeca3bbe3ee40040a053997ebc4987
likeflames <iamwujianhua@163.com>
2026-05-07 21:56:48 +0800

    feat: add Twikoo custom CSS per doc

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -19,6 +19,7 @@ import "vitepress-theme-teek/theme-chalk/tk-fade-up-animation.css"; // 首次加
 import "./styles/code-bg.scss";
 import "./styles/iframe.scss";
 import "./styles/mobile.scss";
+import "./styles/custom.css";
 
 export default {
   extends: Teek,
