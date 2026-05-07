93a9c8b92a13d9b6822168ae0a344f6823ea3ec4
likeflames <iamwujianhua@163.com>
2026-05-07 21:52:08 +0800

    feat: enable Twikoo comments with live backend

---
+++ docs/.vitepress/theme/components/Twikoo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="twikoo"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, watch } from 'vue'
+import { useRoute } from 'vitepress'
+
+const route = useRoute()
+
+const initTwikoo = async () => {
+  // 判断是否在浏览器环境中
+  if (typeof window !== 'undefined') {
+    const twikoo = await import('twikoo')
+    twikoo.init({
+      envId: 'https://twikoo.likeflames.online',
+      el: '#twikoo'
+    })
+  }
+}
+
+// 监听路由刷新评论
+watch(route, () => {
+  initTwikoo()
+})
+
+onMounted(() => {
+  initTwikoo()
+})
+</script>
